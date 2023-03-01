84850cdc8acf392444d7758b81656e262cafba15
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-02-28 22:40:22 -0500

    Update: Put actual code in it

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,7 +1,57 @@
+import { a, useTrail } from '@react-spring/web';
 import React from 'react';
+import projectData from '../data/projects.json';
+import ProjectCardComponent from './ProjectCardComponent';
 
 const ProjectsSection = () => {
-  return <section className="page-section">ProjectsSection</section>;
+  const data = [
+    'HTML5',
+    'CSS3',
+    'Javascript',
+    'Typescript',
+    'React',
+    'Node.js',
+    'Express.js',
+    'MongoDB',
+    'PostgreSQL',
+    'Firebase'
+  ];
+
+  const trail = useTrail(data.length, {
+    from: { height: '100%' },
+    to: { height: '0%' },
+    delay: 700
+  });
+  return (
+    <main className="page-section page-3">
+      <h2>Projects</h2>
+      <article>
+        <div className="projects-section">
+          {projectData.map((project) => (
+            <ProjectCardComponent {...project} />
+          ))}
+        </div>
+        <div className="skills">
+          <h3>My toolkit.</h3>
+          <div>
+            {trail.map((style, i) => (
+              <div className="text" key={i}>
+                / {data[i]} /
+                <a.div
+                  style={{
+                    ...style,
+                    position: 'absolute',
+                    width: '100%',
+                    backgroundColor: '#fcfbf5'
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default ProjectsSection;
