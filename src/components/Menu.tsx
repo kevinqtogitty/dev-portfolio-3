ca9f5a8eeaec72547d1e7c5ea23dac3bb39ba9e6
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-02-28 22:40:58 -0500

    Update: added mobile menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import { a, useSpring, useTrail, useTransition } from '@react-spring/web';
+import PlusIcon from '../assets/icons/plus.svg';
+
+import {
+  a,
+  config,
+  useSpring,
+  useTrail,
+  useTransition
+} from '@react-spring/web';
 
 interface Props {
   setSwipeUp: React.Dispatch<React.SetStateAction<boolean>>;
   setPageKey: React.Dispatch<React.SetStateAction<string>>;
+  swipeUp: boolean;
+  pageKey: string;
 }
 
-const Menu: React.FC<Props> = ({ setSwipeUp, setPageKey }) => {
+const Menu: React.FC<Props> = ({
+  setSwipeUp,
+  setPageKey,
+  swipeUp,
+  pageKey
+}) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   const menu = [
     { name: 'Home', pageKey: 'home' },
-    { name: 'About', pageKey: 'aboute' },
+    { name: 'About', pageKey: 'about' },
     { name: 'Projects', pageKey: 'projects' },
-    { name: 'Contact', pageKey: 'contact' },
-    { name: 'Resume', pageKey: '' }
+    { name: 'Contact', pageKey: 'contact' }
   ];
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   const transition = useTransition(menuIsOpen, {
     from: { y: '50%', opacity: 0 },
@@ -23,7 +37,9 @@ const Menu: React.FC<Props> = ({ setSwipeUp, setPageKey }) => {
     leave: { y: '50%', opacity: 0 }
   });
 
-  const handleSwipe = (e: any) => {
+  const handleSwipe = (e: any, menuType?: string) => {
+    if (menuType) setMenuIsOpen((state) => !state);
+    console.log(e.target.innerText);
     setSwipeUp((state) => !state);
     setTimeout(() => {
       setSwipeUp((state) => !state);
@@ -31,11 +47,71 @@ const Menu: React.FC<Props> = ({ setSwipeUp, setPageKey }) => {
     }, 1000);
   };
 
+  const spring = useSpring({
+    height: menuIsOpen ? '100vh' : '0vh',
+    delay: 150
+  });
+
+  const spring2 = useSpring({
+    height: menuIsOpen ? '100%' : '0%',
+    delay: 300
+  });
+
+  const spring3 = useSpring({
+    y: menuIsOpen ? '0%' : '-150%',
+    opacity: menuIsOpen ? 1 : 0,
+    config: config.slow
+  });
+  const MobileMenu = () => {
+    return (
+      <NavigationMenu.List className="menu-list-mobile">
+        <div className={`menu-trigger-mobile `}>
+          <img
+            src={PlusIcon}
+            alt="open menu plus icon"
+            className={`show-menu-icon ${menuIsOpen ? 'open' : ''} ${
+              pageKey === '02.About' || pageKey === '03.Projects'
+                ? 'light-background'
+                : null
+            }`}
+            onClick={() => setMenuIsOpen((state) => !state)}
+          />
+        </div>
+        <a.div className="links-container" style={spring}>
+          <a.div className="links-box" style={{ ...spring3, zIndex: 1 }}>
+            {menu.map((item, i) => (
+              <NavigationMenu.Item
+                className="menu-item"
+                onClick={(e) => handleSwipe(e, 'l')}
+              >
+                <span>0{i + 1}</span>.{item.name}
+              </NavigationMenu.Item>
+            ))}
+          </a.div>
+          <a.div
+            style={{
+              ...spring2,
+              position: 'absolute',
+              width: '100vw',
+              backgroundColor: '#fcfbf5'
+            }}
+          />
+        </a.div>
+      </NavigationMenu.List>
+    );
+  };
+
   return (
     <NavigationMenu.Root>
-      <NavigationMenu.List className={`menu-list ${menuIsOpen ? 'open' : ''}`}>
+      <NavigationMenu.List
+        className={`menu-list-desktop ${menuIsOpen ? 'open' : ''}`}
+      >
         <NavigationMenu.Item
-          className={`menu-trigger ${menuIsOpen ? 'open' : ''}`}
+          className={`menu-trigger ${menuIsOpen ? 'open' : ''} ${
+            pageKey === '02.About' || pageKey === '03.Projects'
+              ? 'invert-color'
+              : null
+          }`}
           onClick={() => setMenuIsOpen((state) => !state)}
         >
           Menu
@@ -46,8 +122,12 @@ const Menu: React.FC<Props> = ({ setSwipeUp, setPageKey }) => {
             menu.map((menuItem, i) => (
               <a.div style={{ ...style }}>
                 <NavigationMenu.Item
-                  className="link"
-                  value={menu[i].pageKey}
+                  className={`link ${
+                    pageKey === '02.About' || pageKey === '03.Projects'
+                      ? 'invert-color'
+                      : null
+                  }`}
+                  value={menuItem.pageKey}
                   onClick={(e) => handleSwipe(e)}
                   key={i}
                 >
@@ -57,6 +137,41 @@ const Menu: React.FC<Props> = ({ setSwipeUp, setPageKey }) => {
             ))
         )}
       </NavigationMenu.List>
+      <NavigationMenu.List className="menu-list-mobile">
+        <div className={`menu-trigger-mobile `}>
+          <img
+            src={PlusIcon}
+            alt="open menu plus icon"
+            className={`show-menu-icon ${menuIsOpen ? 'open' : ''} ${
+              pageKey === '02.About' || pageKey === '03.Projects'
+                ? 'light-background'
+                : null
+            }`}
+            onClick={() => setMenuIsOpen((state) => !state)}
+          />
+        </div>
+        <a.div className="links-container" style={spring}>
+          <a.div className="links-box" style={{ ...spring3, zIndex: 1 }}>
+            {menu.map((item, i) => (
+              <NavigationMenu.Item
+                key={i}
+                className="menu-item"
+                onClick={(e) => handleSwipe(e, 'l')}
+              >
+                <span>0{i + 1}</span>.{item.name}
+              </NavigationMenu.Item>
+            ))}
+          </a.div>
+          <a.div
+            style={{
+              ...spring2,
+              position: 'absolute',
+              width: '100vw',
+              backgroundColor: '#fcfbf5'
+            }}
+          />
+        </a.div>
+      </NavigationMenu.List>{' '}
     </NavigationMenu.Root>
   );
 };
