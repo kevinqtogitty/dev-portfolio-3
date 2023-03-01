430bcc9dbbcd3b5b500ff33a8399eb9fb1036f09
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-01 18:39:21 -0500

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import './App.css';
+import React, { useEffect, useState } from 'react';
 import { P5Sketch } from './p5.js/p5Functions';
-import { useScroll, animated } from '@react-spring/web';
 import HeroSection from './components/HeroSection';
 import Menu from './components/Menu';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
@@ -25,22 +21,6 @@ function App() {
     };
     window.addEventListener('mousemove', cursor);
   }, []);
-
-  const Cursor = () => {
-    return (
-      <svg viewBox="0 0 120 120" className="cursor">
-        <circle cx="55" cy="55" r="16" className="dotted" />
-        <circle cx="55" cy="55" r="4" className="inner" />
-        <foreignObject
-          x="5"
-          y="5"
-          height="100px"
-          width="100px"
-          className="foreign-object"
-        ></foreignObject>
-      </svg>
-    );
-  };
 
   const menuProps = {
     setSwipeUp,
