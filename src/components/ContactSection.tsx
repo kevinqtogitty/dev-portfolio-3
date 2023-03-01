2b61eb596feaf7de8f727f36017b3b5581abc0f8
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-02-28 22:42:04 -0500

    Update: Put actual code in it

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,7 +1,39 @@
+import { animated, useTrail } from '@react-spring/web';
 import React from 'react';
+import resume from '../assets/toKevinResume.pdf';
 
 const ContactSection = () => {
-  return <section className="page-section">ContactSection</section>;
+  const links = [
+    { text: 'Email', link: 'mailto:kevinq.to@gmail.com' },
+    { text: 'Resume/CV', link: resume },
+    { text: 'Github', link: 'https://www.github.com/kevinqtogitty' },
+    { text: 'LinkedIn', link: 'https://www.linkedin.com/in/kevinqto/' },
+    { text: 'Dev.to', link: 'https://www.dev.to/kevinqtogitty' }
+  ];
+
+  const trail = useTrail(links.length, {
+    from: { x: '-10%', opacity: 0 },
+    to: { x: '0%', opacity: 1 },
+    delay: 700
+  });
+
+  return (
+    <main className="page-section page-4">
+      <ul>
+        {trail.map((style, i) => (
+          <animated.a
+            key={i}
+            style={{ ...style }}
+            href={`${links[i].link}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li>{links[i].text}</li>
+          </animated.a>
+        ))}
+      </ul>
+    </main>
+  );
 };
 
 export default ContactSection;
